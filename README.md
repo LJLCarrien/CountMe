eb0ece3d6ad9a01edff39f67077d2412efc08eb6
ljlcarrien <ljlcarrien@icloud.com>
2021-09-07 20:54:12 +0800

    Add 第一阶段基本功能已完成。下一步是第一阶段的代码优化。 Add 完善README.md；增加json配置描述；增加阶段计划；

--- README.md
+++ README.md
@@ -1,16 +1,277 @@
 # CountMe
-工具：消费统计
+## 背景
 
-环境
-# pip install xlsxwriter
+​	统计我，数据化我，得出结论吧。
 
-第一阶段：表格创建
-todo:周合计、月合计求和
-todo:支持配置excel表格名
-todo:代码优化（部分格式改为支持配置）
+​	将平常的数据留下来，了解自己的消费习惯、消费结构，优化消费结构，提高生活水平，更加了解自己，基于已发生的事实反思，然后有针对性的执行，不断的优化自己的行为模式，看，这漫长的一生，我们最终提交的答卷。
 
-第二阶段：数据存储
-todo:数据保存（json格式)
+## 环境
 
-第三阶段：数据统计
-todo:折线图等,得出结论
+```bash
+pip install xlsxwriter
+```
+
+
+
+## 配置解析 countMe_data.json 
+
+### 配置：标题类别
+
+​	在title里增加行标题类别
+
+举例：增加日期列，name属性是日期。
+
+```json
+{
+	"title": [
+		{
+			"name": "日期",
+		},
+	]
+}
+```
+
+​	
+
+### 配置：字体样式
+
+#### 	列标题字体：titleFontName
+
+```json
+{
+	"titleFontName": "宋体",
+}
+```
+
+### 配置：字体大小
+
+#### 	列标题字体大小：titleFontSize
+
+```json
+{
+	"titleFontSize": 13
+}
+```
+
+#### 	列二级标题字体大小：secTitleFontSize
+
+```json
+{
+	"secTitleFontSize": 11,
+}
+```
+
+
+
+### 配置：宽高
+
+#### 列宽
+
+##### 	默认配置：defaultTitleWidth
+
+##### 	个别配置：width
+
+```json
+{
+	"defaultTitleWidth": 11,
+	"title": [
+		{
+			"name": "ToDo",
+			"width": 20,
+		},
+	]
+}
+```
+
+#### 行高
+
+##### 	默认配置：defaultTitleHeight
+
+```json
+{
+	"defaultTitleHeight": 17,
+}
+```
+
+
+
+### 配置：颜色
+
+#### 列标题字体颜色 fontColor 
+
+- fontColor 默认是黑色，没有该属性就是黑色；
+
+#### 列标题背景颜色 bgColor
+
+- bgColor 默认是无背景颜色，没有该属性即无背景颜色。
+
+#### 举例
+
+- ​	“周末”：白色字体，无背景。（护眼模式可以看到该标题）
+- ​	“ToDo”：红色字体，无背景。
+- ​	“餐饮”：白色字体，背景颜色为#fe4365。
+
+```json
+{
+	"title": [
+		{
+			"name": "周目",
+			"fontColor": "white"
+		},
+		{
+			"name": "ToDo",
+			"fontColor": "red"
+		},
+		{
+			"name": "餐饮",
+			"fontColor": "white",
+			"bgColor": "#fe4365",
+		},
+	],
+}
+```
+
+### 配置：合计 countCell
+
+该列增加合计列，专门用于数字统计
+
+```
+{
+	"title": [
+		{
+			"name": "餐饮",
+			"countCell": "合计",
+		},
+		{
+			"name": "生活用品",
+			"countCell": "合计",
+		},
+		{
+			"name": "交通",
+			"countCell": "合计",
+		},
+		{
+			"name": "住房",
+			"countCell": "合计",
+		}
+	]
+}
+```
+
+
+
+### 配置：行求和
+
+- "rowSumDicKey"-数组：列举所有行求和关键词
+- "rowSumDic"-属性：行求和结果关键字，一般是用来形容该列求和的结果解释
+  对应的关键字做其他列的属性且为true时，表示此列为对应属性行求和时的元素之一。
+
+#### 举例：dayMustCount
+
+​	表示日必须=餐饮+生活用品+交通+住房
+
+```json
+{
+	"title": [
+		{
+			"name": "餐饮",
+            "countCell": "合计",
+			"dayMustCount": true
+		},
+		{
+			"name": "生活用品",
+            "countCell": "合计",
+			"dayMustCount": true
+		},
+		{
+			"name": "交通",
+            "countCell": "合计",
+			"dayMustCount": true
+		},
+		{
+			"name": "住房",
+            "countCell": "合计",
+			"dayMustCount": true
+		},
+		{
+			"name": "日必须",
+			"rowSumDic": "dayMustCount",
+		},
+	],
+	"rowSumDicKey": [
+		"dayMustCount"
+	],
+}
+```
+
+#### 举例：dayCount
+
+​	表示日合计=日必须+娱乐+购物
+
+```json
+{
+	"title": [
+		{
+			"name": "日必须",
+			"dayCount": true
+		},
+		{
+			"name": "娱乐",
+            "countCell": "合计",
+			"dayCount": true
+		},
+		{
+			"name": "购物",
+            "countCell": "合计",
+			"dayCount": true
+		},
+		{
+			"name": "日合计",
+			"rowSumDic": "dayCount",
+		},
+	],
+	"rowSumDicKey": [
+		"dayCount"
+	],
+}
+```
+
+
+
+
+
+## 计划
+
+- 第一阶段：表格创建
+
+  - 基本框架
+    - [x] 配置列宽行高
+    - [x] 列标题
+      - [x] 配置 类别
+      - [x] 配置 字体样式、字体大小、字体颜色、背景颜色
+    - [x] 行日期
+      - [x] 字体样式、字体大小、字体颜色、背景颜色（代码写死/默认，无配置功能）
+    - [x] 行周目
+      - [x] 字体样式、字体大小、字体颜色、背景颜色（代码写死/默认，无配置功能）
+    - [x] 合计
+      - [x] 限制格式，2位小数的数字
+      - [x] 字体样式、字体大小、字体颜色、背景颜色（代码写死/默认，无配置功能）
+
+  - 基本逻辑
+    - [x] 按日期逐行显示
+    - [ ] 一个表格生成全年数据
+
+  - 基本功能
+    - [x] 行求和（日必须、日合计）
+    - [x] 列求和（周合计、月合计）
+  - 代码优化（部分格式改为支持配置）
+    - [ ] 支持配置excel文件名、表格名
+    - [ ] 按功能组织成类
+      - [ ] 格式类
+      - [ ] 读取文件数据为对象
+
+- 第二阶段：数据存储
+  - [ ] todo:数据保存（json格式)
+
+- 第三阶段：数据统计
+  - [ ] todo:折线图等,得出结论
--- coutMe.py
+++ coutMe.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import calendar
 import json
 import xlsxwriter
+import operator
 # 英文简称获取：calendar.day_abbr
 cn_day_abbr = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
 excelSavePath = './test.xlsx'
@@ -11,7 +12,8 @@ obj = calendar.Calendar()
 c_year = datetime.now().year
 c_month = 9
 
-sumDic = {}
+rowSumDic = {}
+colSumDic = {}
 # 合计列下标数组
 countCellList = []
 
@@ -19,10 +21,12 @@ workbook = xlsxwriter.Workbook(excelSavePath)
 worksheet = workbook.add_worksheet(str(c_year))
 
 date_format = workbook.add_format({'num_format': 'm"月"d"日"'})
+date_format.set_font_name('宋体')
 date_format.set_align('center')
 date_format.set_align('vcenter')
 
 weekday_format = workbook.add_format()
+date_format.set_font_name('宋体')
 weekday_format.set_align('left')
 weekday_format.set_align('vcenter')
 
@@ -34,13 +38,13 @@ sum_format.set_font_size(16)
 sum_format.set_font_name('宋体')
 sum_format.set_font_color('red')
 
-countNumCell_format = workbook.add_format()
+# 合计
+countNumCell_format = workbook.add_format({'num_format': '0.00_'})
 countNumCell_format.set_align('center')
 countNumCell_format.set_align('vcenter')
 countNumCell_format.set_font_size(11)
 countNumCell_format.set_font_name('宋体')
 countNumCell_format.set_font_color('red')
-
 
 # print(ord('A'))
 # print(chr(65))
@@ -67,11 +71,11 @@ def getNewTitleFormat(itemInfo, fontSize, fontName):
     return format
 
 
-class SumResult(object):
+class RowSumResult(object):
     def __init__(self):
-        # 求和结果列下标
+        # 求和结果-列下标
         self.index = -1
-        # 用于求和的列下标
+        # 用于求和-列下标
         self.itemList = []
 
     def setIndex(self, i):
@@ -86,16 +90,72 @@ class SumResult(object):
         self.itemList.append(item)
 
 
-def sumDicSetIndex(key, content):
-    if key not in sumDic:
-        sumDic[key] = SumResult()
-    sumDic[key].setIndex(content)
+def rowSumDicSetIndex(key, content):
+    '''
+    行求和字典,列下标设置
+    '''
+    if key not in rowSumDic:
+        rowSumDic[key] = RowSumResult()
+    rowSumDic[key].setIndex(content)
 
 
-def sumDicAddItem(key, listItem):
-    if key not in sumDic:
-        sumDic[key] = SumResult()
-    sumDic[key].addListItem(listItem)
+def rowSumDicAddItem(key, listItem):
+    if key not in rowSumDic:
+        rowSumDic[key] = RowSumResult()
+    rowSumDic[key].addListItem(listItem)
+
+
+class ColSumResult(object):
+    '''
+    列求和
+    '''
+
+    def __init__(self):
+        # 求和结果-列下标
+        self.resultIndex = -1
+        # 求和对象-列下标
+        self.OperatorIndex = -1
+        # 求和对象-行开始
+        self.rowStartIndex = -1
+        # 求和对象-行结束
+        self.rowEndIndex = -1
+
+    def setResultIndex(self, i):
+        self.resultIndex = i
+
+    def setOperatorIndex(self, i):
+        self.OperatorIndex = i
+
+    def setRowStartIndex(self, i):
+        self.rowStartIndex = i
+
+    def setRowEndIndex(self, i):
+        self.rowEndIndex = i
+
+    def getSumStr(self, startIndex, endIndex):
+        self.setRowStartIndex(startIndex)
+        self.setRowEndIndex(endIndex)
+        if self.OperatorIndex != -1:
+            beginStr = rowCol2Str(self.rowStartIndex, self.OperatorIndex)
+            endStr = rowCol2Str(self.rowEndIndex, self.OperatorIndex)
+            return "=SUM(%s:%s)" % (beginStr, endStr)
+        return ""
+
+
+def colSumDicSetIndex(key, funName, content):
+    '''
+    列求和字典,列下标设置
+    '''
+    if key not in colSumDic:
+        colSumDic[key] = ColSumResult()
+    mc = operator.methodcaller(funName, content)
+    mc(colSumDic[key])
+
+
+def getSumStr(key, startIndex, endIndex):
+    if key not in colSumDic:
+        colSumDic[key] = ColSumResult()
+    return colSumDic[key].getSumStr(startIndex, endIndex)
 
 
 def main():
@@ -104,24 +164,27 @@ def main():
         listIndex = 0
 
         jsonData = json.load(f)
-        defaultTitleFontSize = jsonData['defaultTitleFontSize']
-        defaultTitleFontName = jsonData['defaultTitleFontName']
-        defaultSecTitleFontSize = jsonData['defaultSecTitleFontSize']
+        titleFontSize = jsonData['titleFontSize']
+        titleFontName = jsonData['titleFontName']
+        secTitleFontSize = jsonData['secTitleFontSize']
         secondTitle = jsonData['secondTitle']
         titleInfo = jsonData['title']
-        sumDicKey = jsonData['sumDicKey']
+        defaultTitleHeight = jsonData['defaultTitleHeight']
+        # 行求和
+        rowSumDicKey = jsonData['rowSumDicKey']
+        colSumDicKey = jsonData['colSumDicKey']
 
         for titleItem in titleInfo:
             value = titleItem['name']
             bHaveSecMenu = 'secondSort' in titleItem
 
             tmp_title_format = getNewTitleFormat(
-                titleItem, defaultTitleFontSize, defaultTitleFontName)
+                titleItem, titleFontSize, titleFontName)
 
             # 含二级菜单
             if bHaveSecMenu:
                 tmp_Sec_title_format = getNewTitleFormat(
-                    titleItem, defaultSecTitleFontSize, defaultTitleFontName)
+                    titleItem, secTitleFontSize, titleFontName)
 
                 oldlistIndex = listIndex
                 secondMenuList = secondTitle[titleItem['secondSort']]
@@ -147,63 +210,111 @@ def main():
             worksheet.set_column(listIndex, listIndex, colWidth)
 
             # 行高
-            rowHeight = jsonData['defaultTitleHeight']
-            worksheet.set_row(lineIndex, rowHeight)
+            worksheet.set_row(lineIndex, defaultTitleHeight)
 
             if 'countCell' in titleItem:
                 listIndex = listIndex+1
                 countCellList.append(listIndex)
                 worksheet.merge_range(lineIndex, listIndex, lineIndex+1, listIndex,
                                       titleItem['countCell'], tmp_title_format)
+            '''
+            行求和
+            '''
+            if 'rowSumDic' in titleItem:
+                key = titleItem['rowSumDic']
+                rowSumDicSetIndex(key, listIndex)
 
-            if 'sumDic' in titleItem:
-                key = titleItem['sumDic']
-                sumDicSetIndex(key, listIndex)
+            for key in rowSumDicKey:
+                if key in titleItem and titleItem[key]:
+                    rowSumDicAddItem(key, listIndex)
 
-            for key in sumDicKey:
-                if key in titleItem:
-                    sumDicAddItem(key, listIndex)
+            '''
+            列求和
+            '''
+            if 'colSumDic' in titleItem:
+                key = titleItem['colSumDic']
+                colSumDicSetIndex(key, 'setResultIndex', listIndex)
 
+            for key in colSumDicKey:
+                if key in titleItem and titleItem[key]:
+                    colSumDicSetIndex(key, 'setOperatorIndex', listIndex)
             listIndex = listIndex+1
-
     maxlistIndex = listIndex-1
-    # 日期&周目
-    lineIndex = lineIndex+1
-    for day in obj.itermonthdays4(c_year, c_month):
-        if not day[1] == c_month:
-            continue
 
+    # 日期&周目
+    newWeek = True
+    curdayIndex = 0
+    montTotoalDayNum = calendar.monthrange(c_year, c_month)[1]
+    
+    weekStartLineIndex=0
+    weekEndLineIndex = 0
+
+    montStartLineIndex=0
+    montEndLineIndex = 0
+
+    lineIndex = lineIndex+1+len(secondTitle)
+    for dayItem in obj.itermonthdays4(c_year, c_month):
+        if not dayItem[1] == c_month:
+            continue
+        if curdayIndex == 0:
+            montStartLineIndex = lineIndex
+        if newWeek:
+            weekStartLineIndex = lineIndex
+            newWeek = False
         # 日期：年月日，显示格式:x月x日
-        yearMontDay = "%d-%d-%d" % (c_year, day[1], day[2])
+        yearMontDay = "%d-%d-%d" % (c_year, dayItem[1], dayItem[2])
         date = datetime.strptime(yearMontDay, "%Y-%m-%d")
         worksheet.write_datetime(lineIndex, 0, date, date_format)
 
         # 周数
-        cnWeekNumStr = cn_day_abbr[day[3]]
+        cnWeekNumStr = cn_day_abbr[dayItem[3]]
         worksheet.write(lineIndex, 1, cnWeekNumStr, weekday_format)
+        curdayIndex = curdayIndex+1
 
-        # 求和
-        for sumItem in sumDic.keys():
+        # 行求和
+        for sumItem in rowSumDic.keys():
             sumItemList = []
             for tmpLie in range(maxlistIndex):
                 for lie in countCellList:
                     if tmpLie == lie:
-                        worksheet.write(lineIndex, tmpLie, "", countNumCell_format)
-                for needCountLie in sumDic[sumItem].itemList:
+                        # 求和格式
+                        worksheet.write(lineIndex, tmpLie, "",countNumCell_format)
+                for needCountLie in rowSumDic[sumItem].itemList:
                     if tmpLie == needCountLie:
                         sumStr = rowCol2Str(lineIndex, tmpLie)
                         sumItemList.append(sumStr)
             sumStr = ",".join(sumItemList)
             sumStr = "=SUM(%s)" % sumStr
             worksheet.write(
-                lineIndex, sumDic[sumItem].index, sumStr, sum_format)
+                lineIndex, rowSumDic[sumItem].index, sumStr, sum_format)
 
-        # 换行
+        # 周求和
         if cnWeekNumStr == '周日':
+            weekEndLineIndex = lineIndex
+            sumStr = getSumStr('weekCount', weekStartLineIndex, weekEndLineIndex)
             lineIndex = lineIndex+1
+            worksheet.write(lineIndex, colSumDic['weekCount'].resultIndex, sumStr, sum_format)
+            newWeek = True
+        #  月求和
+        if curdayIndex == montTotoalDayNum:
+            weekEndLineIndex = lineIndex
+            montEndLineIndex = lineIndex
+
+            if cnWeekNumStr != '周日':
+                sumStr = getSumStr('weekCount', weekStartLineIndex, weekEndLineIndex)
+                lineIndex = lineIndex+1
+                worksheet.write(lineIndex, colSumDic['weekCount'].resultIndex, sumStr, sum_format)
+
+            sumStr = getSumStr('weekCount', montStartLineIndex, montEndLineIndex)
+            lineIndex = lineIndex+1
+            worksheet.write(
+                lineIndex, colSumDic['MonthCount'].resultIndex, sumStr, sum_format)
+            newWeek = True
+        worksheet.set_row(lineIndex, defaultTitleHeight)
         lineIndex = lineIndex+1
 
     workbook.close()
+
 
 if __name__ == "__main__":
     main()
