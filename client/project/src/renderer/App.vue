4d9c898486ebfabee4870b50207c1b808df9e644
ljlcarrien <ljlcarrien@icloud.com>
2022-01-05 15:16:22 +0800

    Add 项目初始化

---
+++ client/project/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'countme_client'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
