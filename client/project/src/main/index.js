6ab3dbd718ae3339151bc72f0347412474acafd2
ljlcarrien <ljlcarrien@icloud.com>
2022-01-10 20:32:28 +0800

    Add 打通前端生成json数据流程

--- client/project/src/main/index.js
+++ client/project/src/main/index.js
@@ -1,5 +1,9 @@
-import { app, BrowserWindow } from 'electron'
+import console from 'console'
+import { app, BrowserWindow, ipcMain } from 'electron'
+import path from 'path'
 import '../renderer/store'
+
+const fs = require("fs")
 
 /**
  * Set `__static` path to static files in production
@@ -14,7 +18,29 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
-function createWindow () {
+let jsonInfoObj
+
+app.on('ready', onReady)
+function onReady() {
+  createWindow();
+  readFile();
+  writeFile();
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+function createWindow() {
+
   /**
    * Initial window options
    */
@@ -29,21 +55,18 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+
+
 }
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+ipcMain.on('pageFinishInit', (event, arg) => {
+  //这里是渲染进程发送来的消息
+  console.log('----------------------pageFinishInit', arg);
+  if (mainWindow != null) {
+    mainWindow.webContents.send('initTitle', jsonInfoObj['title'], jsonInfoObj['secondTitle']);
   }
-})
 
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+});
 
 /**
  * Auto Updater
@@ -64,3 +87,32 @@ app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
  */
+
+
+function readFile() {
+  const filepath = path.join(__dirname, '../../../../config.json').replace(/\\/g, "\/");
+  const exists = fs.existsSync(filepath);
+  console.log(filepath);
+  if (exists) {
+    jsonInfoObj = JSON.parse(fs.readFileSync(filepath));
+  }
+  else {
+    console.log(filepath, '文件不存在');
+  }
+}
+ipcMain.on('onSaveJson', (event, arg) => {
+  writeFile(arg)
+});
+function writeFile(jsonObj) {
+
+  // convert JSON object to string
+  const data = JSON.stringify(jsonObj);
+
+  // write JSON string to a file
+  fs.writeFile('user.json', data, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("JSON data is saved.");
+  });
+}
--- client/project/src/renderer/components/LandingPage.vue
+++ client/project/src/renderer/components/LandingPage.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
       <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
         <system-information></system-information>
+        <div class="postData" @click="getAllData">测试</div>
+        <span> {{ titleInfo }}</span>
       </div>
 
       <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
+        <div>
+          <ul class="title" v-for="title in titleInfo" :key="title.name">
+            {{
+              title.showname
+            }}
+            <!-- 二级菜单 -->
+            <div v-if="title.secList != null">
+              <ul
+                class="sectitle"
+                v-for="sectitle in title.secList"
+                :key="sectitle.name"
+              >
+                {{
+                  sectitle.showname
+                }}
+                <input id="sectitle_des" v-model="sectitle.des" />
+              </ul>
+            </div>
+            <!-- 一级菜单内容描述 -->
+            <div v-if="title.des != undefined" id="title_des">
+              <input id="title_des" v-model="title.des" />
+            </div>
+            <!-- 一级菜单合计 -->
+            <div v-if="title.countcell != undefined" id="title_countcell">
+              <input type="number" v-model="title.countcell" />
+            </div>
+          </ul>
         </div>
       </div>
     </main>
@@ -30,99 +42,234 @@
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
+import SystemInformation from "./LandingPage/SystemInformation";
+const { ipcRenderer } = require("electron");
 
-  export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
+export default {
+  name: "landing-page",
+  components: { SystemInformation },
+  data() {
+    return {
+      pageName: "LandingPage",
+      titleInfo: [],
+    };
+  },
+  watch: {
+    // titleInfo: function () {
+    //   console.log(this.titleInfo);
+    // },
+  },
+  beforeCreate() {
+    console.log("beforecreate:");
+  },
+  created() {
+    console.log("create:");
+    console.log(this.pageName);
+    ipcRenderer.send("pageFinishInit", this.pageName);
+  },
+  beforeMount() {
+    console.log("beforeMount:");
+  },
+  mounted() {
+    console.log("mounted:");
+
+    ipcRenderer.on("initTitle", (event, title, secTitles) => {
+      this.titleInfo = this.handleTitle(title, secTitles);
+    });
+  },
+  methods: {
+    getTitleObj: function (item, type) {
+      let obj = new Object({
+        name: item.name,
+        showname: item.showname,
+      });
+      if (type == "des") {
+        obj["des"] = ""; //内容描述
+      } else if (type == "countcell") {
+        obj["countcell"] = 0; ///合计数值
+      } else {
+        obj["des"] = ""; //内容描述
+        obj["countcell"] = 0; ///合计数值
       }
-    }
-  }
+      return obj;
+    },
+    getSecTitleByTitleName: function (titleName, secTitles) {
+      let result = null;
+      for (const secTitleParentName in secTitles) {
+        if (titleName == secTitleParentName) {
+          if (result == null) {
+            result = [];
+          }
+          result = secTitles[secTitleParentName];
+        }
+      }
+      return result;
+    },
+    //处理json数据-二级菜单数据
+    handleSecTitle: function (secTitleList) {
+      if (secTitleList == null) {
+        return null;
+      }
+      let result = [];
+      for (const index in secTitleList) {
+        let item = secTitleList[index];
+        let secObj = this.getTitleObj(item, "des");
+        result.push(secObj);
+      }
+      return result;
+    },
+    //处理json数据
+    handleTitle: function (titles, secTitles) {
+      let result = [];
+      if (titles != null) {
+        for (const index in titles) {
+          let title_item = titles[index];
+
+          let secTitleList = this.getSecTitleByTitleName(
+            title_item.name,
+            secTitles
+          );
+          let obj =
+            secTitleList != null
+              ? this.getTitleObj(title_item, "countcell")
+              : title_item.addCountCell
+              ? this.getTitleObj(title_item, "both")
+              : title_item.countcell
+              ? this.getTitleObj(title_item, "countcell")
+              : this.getTitleObj(title_item, "des");
+          let secList = this.handleSecTitle(secTitleList);
+          obj["secList"] = secList;
+          result.push(obj);
+        }
+      }
+      return result;
+    },
+    getAllData: function () {
+      let result = this.makeSaveJsonData(this.titleInfo);
+      console.log(result);
+      ipcRenderer.send("onSaveJson", result);
+    },
+    makeSaveJsonData: function (info) {
+      let result = new Object();
+      let obj = null;
+      for (const index in info) {
+        let item = info[index];
+        if (item.name == "date") {
+          result[item.des] = new Object();
+          obj = result[item.des];
+        } else if (item.name == "weekday") {
+          continue;
+        } else {
+          let undefinedDes = item.des == undefined;
+          let nullDes = item.des == "";
+          let undefinedCountcell = item.countcell == undefined;
+          let zeroCountcell = item.countcell == 0;
+          let bSecList = item.secList != undefined && item.secList != null;
+          if (!undefinedDes && !nullDes) {
+            // 有描述-无合计
+            if (undefinedCountcell) {
+              console.log(item.name, item.des, obj);
+              obj[item.name] = item.des;
+            }
+            // 有描述-有合计-无二级菜单
+            else if (!bSecList) {
+              obj[item.name] = new Object({
+                des: item.des,
+                countcell: parseInt(item.countcell),
+              });
+            }
+          } else {
+            //无描述-有二级菜单-有合计
+            if (bSecList) {
+              let secObj = new Object();
+              let secList = item.secList;
+              for (const key in secList) {
+                const element = secList[key];
+                if (element.des != undefined && element.des != "") {
+                  secObj[element.name] = element.des;
+                }
+              }
+              secObj["countcell"] = parseInt(item.countcell);
+              obj[item.name] = secObj;
+            } else {
+              //无描述-无二级菜单-有合计 存在才记录
+              if (!undefinedCountcell && !zeroCountcell) {
+                obj[item.name] = new Object({
+                  countcell: parseInt(item.countcell),
+                });
+              }
+            }
+          }
+        }
+      }
+      return result;
+    },
+  },
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+body {
+  font-family: "Source Sans Pro", sans-serif;
+}
 
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
+#wrapper {
+  background: radial-gradient(
+    ellipse at top left,
+    rgba(255, 255, 255, 1) 40%,
+    rgba(229, 229, 229, 0.9) 100%
+  );
+  height: 100vh;
+  padding: 60px 80px;
+  width: 100vw;
+}
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
+#logo {
+  height: auto;
+  margin-bottom: 20px;
+  width: 420px;
+}
 
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
+main {
+  display: flex;
+  justify-content: space-between;
+}
 
-  main > div { flex-basis: 50%; }
+main > div {
+  flex-basis: 50%;
+}
 
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
+.left-side {
+  display: flex;
+  flex-direction: column;
+}
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
+.title {
+  color: #2c3e50;
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 6px;
+}
+.sectitle {
+  margin-left: 20px;
+  margin-top: 6px;
+}
 
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
-  }
+.postData {
+  width: 150px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  border: 1px solid #ccc;
+  text-align: center;
+  cursor: pointer;
+}
 </style>
